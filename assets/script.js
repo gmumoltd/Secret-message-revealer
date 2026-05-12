a9ca519f28f9eae0664e203800526540724de217
unknown <gmumoltd@gmail.com>
2026-05-12 15:05:22 +0300

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+document.getElementsByTagName("btn")[0].innerHTML = "<h2>JavaScript is Powerful!</h2>"
+
+document.getElementsByTagName("btn")[0].innerHTML = "<h2>This is innertext</h2>"
+
+document.getElementsByTagName("btn")[0].innerHTML = "<h2>this is textConcent</h2>"
